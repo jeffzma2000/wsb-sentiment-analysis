2d394181087cb971728b04278e62c3518a6454c0
jeffzma2000 <jeffrey.ma.jzm5@yale.edu>
2020-08-30 14:47:25 -0400

    added vader sentiment analysis

--- wsb_scraper.py
+++ wsb_scraper.py
@@ -1,23 +1,36 @@
 import praw
 import pandas as pd
 import os
+import datetime
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 reddit = praw.Reddit('bot1', user_agent='jeffzma2000 wsb sentiment analysis script')
 wsb = reddit.subreddit('wallstreetbets')
 
-def scrape_for(symbol, limit, subreddit=wsb):
+def get_sentiment(df_col):
+    vader = SentimentIntensityAnalyzer()
+    scores = df_col.apply(vader.polarity_scores).tolist()
+    return pd.DataFrame(scores)
 
-    result = {}
+def get_date(submission):
+    time = submission.created
+    return datetime.datetime.fromtimestamp(time)
+
+def scrape_for(symbol, limit=None, subreddit=wsb):
+
+    result = []
 
     for submission in wsb.hot(limit=limit):
+        date = get_date(submission)
         for word in submission.title.split():
             if word.lower() == symbol.lower():
-                result['title'] = submission.title
-                submission.comments.replace_more(limit=None)
-                for comment in submission.comments.list():
-                    print(comment.body)
+                result.append((submission.title, date))
+                submission.comments.replace_more(limit=0)
+                for top_level_comment in submission.comments:
+                    result.append((top_level_comment.body, date))
                 break
+    df = pd.DataFrame(result, columns=['Text', 'Date'])
+    scored_df = df.join(get_sentiment(df['Text']), rsuffix='_right')
+    return scored_df
 
-    return result
-
-print(scrape_for('tsla', 30))
+# print(scrape_for('tsla', 5))
